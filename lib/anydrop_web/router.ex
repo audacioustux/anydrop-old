468485eca1d79427bd13a6792d3c2796ba1ac565
a-0vi <ovidas98@gmail.com>
2024-06-03 23:41:35 +0600

    add route for new home page

--- lib/anydrop_web/router.ex
+++ lib/anydrop_web/router.ex
@@ -17,7 +17,7 @@ defmodule AnydropWeb.Router do
   scope "/", AnydropWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", HomeLive
   end
 
   # Other scopes may use custom stacks.
