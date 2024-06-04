76c4b8df3716999f78a9069ce5d3e533b09bf71c
a-0vi <ovidas98@gmail.com>
2024-06-04 22:21:03 +0600

    refactor button again

--- lib/anydrop_web/live/home_live.ex
+++ lib/anydrop_web/live/home_live.ex
@@ -12,16 +12,18 @@ defmodule AnydropWeb.HomeLive do
       <.input type="textarea" field={@form[:body]}  class="h-64 !text-xl bg-[#E6E6E6] font-semibold" required  />
       <div class="flex justify-end">
         <button disable-with="Dropping..."
-          class="
-                rounded-md text-2xl font-medium leading-5 text-slate-700 hover:text-slate-800 active:fill-slate-900
-                fill-slate-700 hover:fill-slate-800 active:fill-slate-900
-                shadow-sm ring-1 ring-slate-700/10 bg-white hover:bg-slate-50 active:bg-slate-100
-                "
+                class="bg-white inline-flex items-center justify-center whitespace-nowrap rounded-md
+                        text-sm font-medium ring-offset-background transition-colors
+                        focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
+                        focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50
+                        h-10 px-8 py-4
+                        text-slate-700 hover:text-slate-800 active:fill-text-900
+                        fill-slate-700 hover:fill-slate-800 active:fill-slate-900
+                        shadow-md ring-1 ring-slate-700/10 bg-white hover:bg-slate-50 active:bg-slate-100
+                      "
         >
-          <div class="flex items-center justify-around px-3 py-2">
-            <svg class="mr-2 h-10 w-10 flex-none " stroke="currentColor" stroke-width="0" viewBox="0 0 32 32" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M 28 16 C 28 9.382813 22.617188 4 16 4 C 9.382813 4 4 9.382813 4 16 L 4 16.453125 L 12 23.453125 L 12 28 L 20 28 L 20 23.453125 L 28 16.453125 Z M 16 6.363281 C 16.867188 7.304688 18.421875 9.535156 18.871094 13.65625 C 18.148438 13.28125 17.207031 13 16 13 C 14.792969 13 13.851563 13.28125 13.128906 13.65625 C 13.578125 9.53125 15.136719 7.300781 16 6.363281 Z M 11.085938 16.398438 L 13.484375 22 L 13.375 22 L 6.335938 15.839844 C 6.71875 15.449219 7.390625 15 8.5 15 C 10.402344 15 11.058594 16.351563 11.085938 16.398438 Z M 15.660156 22 L 13.140625 16.125 C 13.523438 15.691406 14.378906 15 16 15 C 17.625 15 18.484375 15.695313 18.859375 16.121094 L 16.339844 22 Z M 18.515625 22 L 20.914063 16.398438 C 20.925781 16.382813 21.554688 15 23.5 15 C 24.601563 15 25.273438 15.453125 25.660156 15.84375 L 18.625 22 Z M 25.660156 13.445313 C 25.078125 13.179688 24.367188 13 23.5 13 C 22.390625 13 21.535156 13.292969 20.878906 13.683594 C 20.535156 10.207031 19.46875 7.824219 18.476563 6.320313 C 21.976563 7.21875 24.738281 9.957031 25.660156 13.445313 Z M 13.519531 6.324219 C 12.53125 7.824219 11.464844 10.207031 11.121094 13.683594 C 10.464844 13.292969 9.609375 13 8.5 13 C 7.632813 13 6.921875 13.179688 6.339844 13.445313 C 7.261719 9.957031 10.023438 7.21875 13.519531 6.324219 Z M 18 26 L 14 26 L 14 24 L 18 24 Z"></path></svg>
-            <span class="mr-1">Drop</span>
-          </div>
+          <svg class="mr-2 w-6 h-6"  stroke-width="0" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M1 1.91L1.78 1.5L15 7.44899V8.3999L1.78 14.33L1 13.91L2.58311 8L1 1.91ZM3.6118 8.5L2.33037 13.1295L13.5 7.8999L2.33037 2.83859L3.6118 7.43874L9 7.5V8.5H3.6118Z"></path></svg>
+          Drop Now !
         </button>
       </div>
     </.simple_form>
